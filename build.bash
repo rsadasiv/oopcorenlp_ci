c4181865a6c66ca817fe5e9b9a26c9bdfcddef85
Ram Sadasiv <io.outofprintmagazine@gmail.com>
2020-12-06 18:08:57 -0500

    latest recipe

--- build.bash
+++ build.bash
@@ -26,18 +26,18 @@ then
 	tar xvfz verbnet-3.3.tar.gz
 fi
 
-
+echo "build jverbnet"
 #build jverbnet from bugfix branch
 cd $OOP_HOME
 if [ ! -d jverbnet ]
 then
 	git clone https://github.com/rsadasiv/jverbnet.git --branch bugfix/verbnet33 --single-branch
-fi	
+fi
 cd jverbnet
 git pull
-mvn install
+#mvn install
 
-
+echo "build oopcorenlp_parent"
 #build oopcorenlp_parent
 cd $OOP_HOME
 if [ ! -d oopcorenlp_parent ]
@@ -46,8 +46,9 @@ then
 fi
 cd oopcorenlp_parent
 git pull
-mvn clean install site
+#mvn clean install site
 
+echo "build oopcorenlp"
 #build oopcorenlp
 cd $OOP_HOME
 if [ ! -d oopcorenlp ]
@@ -56,8 +57,9 @@ then
 fi
 cd oopcorenlp
 git pull
-mvn clean install site
+#mvn clean install site
 
+echo "build oopcorenlp_corpus"
 #build oopcorenlp_corpus
 cd $OOP_HOME
 if [ ! -d oopcorenlp_corpus ]
@@ -66,41 +68,31 @@ then
 fi
 cd oopcorenlp_corpus
 git pull
-mvn clean install site
+#mvn clean install site
 
+echo "build oopcorenlp_corpus_cli"
 #build oopcorenlp_corpus_cli
 cd $OOP_HOME
-git clone https://github.com/rsadasiv/oopcorenlp_corpus_cli.git
+if [ ! -d oopcorenlp_corpus_cli ]
+then
+	git clone https://github.com/rsadasiv/oopcorenlp_corpus_cli.git
+fi
 cd oopcorenlp_corpus_cli
-mvn install site
+git pull
+#mvn install site
 
-
-#oopcorenlp.properties must exist in ./Sample/
+echo "run oopcorenlp_corpus_cli"
 #run cli
 cd $OOP_HOME
-if [ ! -d Sample ]
-then
-	mkdir Sample
-	cd Sample
-	mkdir Chekhov
-	mkdir Maupassant
-	mkdir Wodehouse
-	mkdir OHenry
-else
-	rm -Rf ./Sample/Chekhov/*
-	rm -Rf ./Sample/Maupassant/*
-	rm -Rf ./Sample/Wodehouse/*
-	rm -Rf ./Sample/OHenry/*
-fi
+# java -Xms8096m -Xmx10120m -jar oopcorenlp_corpus_cli/target/oopcorenlp_corpus_cli-$CLI_VERSION.jar --action generate --outputPath ./Sample
+# java -Xms8096m -Xmx10120m -jar oopcorenlp_corpus_cli/target/oopcorenlp_corpus_cli-$CLI_VERSION.jar --action analyze --inputPath ./Sample/ChekhovBatch.json
+# java -Xms8096m -Xmx10120m -jar oopcorenlp_corpus_cli/target/oopcorenlp_corpus_cli-$CLI_VERSION.jar --action analyze --inputPath ./Sample/MaupassantBatch.json
+# java -Xms8096m -Xmx10120m -jar oopcorenlp_corpus_cli/target/oopcorenlp_corpus_cli-$CLI_VERSION.jar --action analyze --inputPath ./Sample/WodehouseBatch.json
+# java -Xms8096m -Xmx10120m -jar oopcorenlp_corpus_cli/target/oopcorenlp_corpus_cli-$CLI_VERSION.jar --action analyze --inputPath ./Sample/OHenryBatch.json
 
-cd $OOP_HOME
-java -Xms8096m -Xmx10120m -jar oopcorenlp_corpus_cli/target/oopcorenlp_corpus_cli-$CLI_VERSION.jar --action generate --outputPath ./Sample
-java -Xms8096m -Xmx10120m -jar oopcorenlp_corpus_cli/target/oopcorenlp_corpus_cli-$CLI_VERSION.jar --action analyze --inputPath ./Sample/ChekhovBatch.json
-java -Xms8096m -Xmx10120m -jar oopcorenlp_corpus_cli/target/oopcorenlp_corpus_cli-$CLI_VERSION.jar --action analyze --inputPath ./Sample/MaupassantBatch.json
-java -Xms8096m -Xmx10120m -jar oopcorenlp_corpus_cli/target/oopcorenlp_corpus_cli-$CLI_VERSION.jar --action analyze --inputPath ./Sample/WodehouseBatch.json
-java -Xms8096m -Xmx10120m -jar oopcorenlp_corpus_cli/target/oopcorenlp_corpus_cli-$CLI_VERSION.jar --action analyze --inputPath ./Sample/OHenryBatch.json
 
-#build oopcorenlp_web
+echo "download oopcorenlp_web"
+#download oopcorenlp_web
 cd $OOP_HOME
 if [ ! -d oopcorenlp_web ]
 then
@@ -109,24 +101,72 @@ fi
 cd oopcorenlp_web
 git pull
 
+echo "deploy oopcorenlp_corpus output to ooopcorenlp_web"
 #deploy Analyze output to tomcat
-mkdir oopcorenlp_web/WebContent/Corpora/Chekhov
-cp ./Sample/Corpora/Gutenberg/Chekhov/Analyze/* oopcorenlp_web/WebContent/Corpora/Chekhov/
-mkdir oopcorenlp_web/WebContent/Corpora/Maupassant
-cp ./Sample/Corpora/Gutenberg/Maupassant/Analyze/* oopcorenlp_web/WebContent/Corpora/Maupassant/
-mkdir oopcorenlp_web/WebContent/Corpora/Wodehouse
-cp ./Sample/Corpora/EBook/Wodehouse/Analyze/* oopcorenlp_web/WebContent/Corpora/Wodehouse/
-mkdir oopcorenlp_web/WebContent/Corpora/OHenry
-cp ./Sample/Corpora/Wikisource/OHenry/Analyze/* oopcorenlp_web/WebContent/Corpora/OHenry/
+# cd $OOP_HOME
+# #Chekhov
+# mkdir -p oopcorenlp_web/WebContent/Corpora/Chekhov
+# cp ./Sample/Corpora/Gutenberg/Chekhov/Analyze/* oopcorenlp_web/WebContent/Corpora/Chekhov/
+# cp ./Sample/Corpora/Gutenberg/Chekhov/CorpusAggregate/* oopcorenlp_web/WebContent/Corpora/Chekhov/
+# cp ./Sample/Corpora/Gutenberg/Chekhov/ChekhovBatch.json oopcorenlp_web/WebContent/Corpora/Chekhov/
+#
+# mkdir -p oopcorenlp_web/WebContent/Corpora/Chekhov/CoreNLPTfidf
+# cp -r ./Sample/Corpora/Gutenberg/Chekhov/CoreNLPTfidf oopcorenlp_web/WebContent/Corpora/Chekhov/CoreNLPTfidf
+# mkdir -p oopcorenlp_web/WebContent/Corpora/Chekhov/CoreNLPZ
+# cp -r ./Sample/Corpora/Gutenberg/Chekhov/CoreNLPZ oopcorenlp_web/WebContent/Corpora/Chekhov/CoreNLPZ
+# mkdir -p oopcorenlp_web/WebContent/Corpora/Chekhov/Word2Vec
+# cp -r ./Sample/Corpora/Gutenberg/Chekhov/Word2Vec oopcorenlp_web/WebContent/Corpora/Chekhov/Word2Vec
+#
+# #Maupassant
+# mkdir -p oopcorenlp_web/WebContent/Corpora/Maupassant
+# cp ./Sample/Corpora/Gutenberg/Maupassant/Analyze/* oopcorenlp_web/WebContent/Corpora/Maupassant/
+# cp ./Sample/Corpora/Gutenberg/Maupassant/CorpusAggregate/* oopcorenlp_web/WebContent/Corpora/Maupassant/
+# cp ./Sample/Corpora/Gutenberg/Maupassant/MaupassantBatch.json oopcorenlp_web/WebContent/Corpora/Maupassant/
+#
+# mkdir -p oopcorenlp_web/WebContent/Corpora/Maupassant/CoreNLPTfidf
+# cp -r ./Sample/Corpora/Gutenberg/Maupassant/CoreNLPTfidf oopcorenlp_web/WebContent/Corpora/Maupassant/CoreNLPTfidf
+# mkdir -p oopcorenlp_web/WebContent/Corpora/Maupassant/CoreNLPZ
+# cp -r ./Sample/Corpora/Gutenberg/Maupassant/CoreNLPZ oopcorenlp_web/WebContent/Corpora/Maupassant/CoreNLPZ
+# mkdir -p oopcorenlp_web/WebContent/Corpora/Maupassant/Word2Vec
+# cp -r ./Sample/Corpora/Gutenberg/Maupassant/Word2Vec oopcorenlp_web/WebContent/Corpora/Maupassant/Word2Vec
+#
+# #Wodehouse
+# mkdir -p oopcorenlp_web/WebContent/Corpora/Wodehouse
+# cp ./Sample/Corpora/EBook/Wodehouse/Analyze/* oopcorenlp_web/WebContent/Corpora/Wodehouse/
+# cp ./Sample/Corpora/EBook/Wodehouse/CorpusAggregate/* oopcorenlp_web/WebContent/Corpora/Wodehouse/
+# cp ./Sample/Corpora/EBook/Wodehouse/WodehouseBatch.json oopcorenlp_web/WebContent/Corpora/Wodehouse/
+#
+# mkdir -p oopcorenlp_web/WebContent/Corpora/Wodehouse/CoreNLPTfidf
+# cp -r ./Sample/Corpora/EBook/Wodehouse/CoreNLPTfidf oopcorenlp_web/WebContent/Corpora/Wodehouse/CoreNLPTfidf
+# mkdir -p oopcorenlp_web/WebContent/Corpora/Wodehouse/CoreNLPZ
+# cp -r ./Sample/Corpora/EBook/Wodehouse/CoreNLPZ oopcorenlp_web/WebContent/Corpora/Wodehouse/CoreNLPZ
+# mkdir -p oopcorenlp_web/WebContent/Corpora/Wodehouse/Word2Vec
+# cp -r ./Sample/Corpora/EBook/Wodehouse/Word2Vec oopcorenlp_web/WebContent/Corpora/Wodehouse/Word2Vec
+#
+# #OHenry
+# mkdir -p oopcorenlp_web/WebContent/Corpora/OHenry
+# cp ./Sample/Corpora/Wikisource/OHenry/Analyze/* oopcorenlp_web/WebContent/Corpora/OHenry/
+# cp ./Sample/Corpora/Wikisource/OHenry/CorpusAggregate/* oopcorenlp_web/WebContent/Corpora/OHenry/
+# cp ./Sample/Corpora/Wikisource/OHenry/OHenryBatch.json oopcorenlp_web/WebContent/Corpora/OHenry/
+#
+# mkdir -p oopcorenlp_web/WebContent/Corpora/OHenry/CoreNLPTfidf
+# cp -r ./Sample/Corpora/Wikisource/OHenry/CoreNLPTfidf oopcorenlp_web/WebContent/Corpora/OHenry/CoreNLPTfidf
+# mkdir -p oopcorenlp_web/WebContent/Corpora/OHenry/CoreNLPZ
+# cp -r ./Sample/Corpora/Wikisource/OHenry/CoreNLPZ oopcorenlp_web/WebContent/Corpora/OHenry/CoreNLPZ
+# mkdir -p oopcorenlp_web/WebContent/Corpora/OHenry/Word2Vec
+# cp -r ./Sample/Corpora/Wikisource/OHenry/Word2Vec oopcorenlp_web/WebContent/Corpora/OHenry/Word2Vec
 
+
+echo "build ooopcorenlp_web"
 #build oopcorenlp
 cd $OOP_HOME
 cd oopcorenlp_web
-mvn clean install site
+mvn -DskipITs clean install site
 
 #corpus cleanup
 #rm -Rf oopcorenlp_web/WebContent/Corpora/*
 
+echo "publish docs"
 #publish docs
 cd $OOP_HOME
 if [ ! -d rsadasiv.github.io ]

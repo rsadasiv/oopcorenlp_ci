6a119a05fb600b948809b01d9e3ebe31fb4f9460
= <=>
2020-12-06 11:41:57 -0500

    build.bash

--- build.bash
+++ build.bash
@@ -1,62 +1,137 @@
+#!/bin/bash
 MAVEN_VERSION=3.6.3
 TOMCAT_VERSION=9.0.36
 CLI_VERSION=1.0
-OOP_HOME=$PWD
+OOP_HOME=$PWD/..
 
+set -e
 
 #install wordnet, verbnet from download
 cd $OOP_HOME
-if [! -d data ]
+if [ ! -d data ]
 then
 	mkdir data
 fi
 
 cd data
-if [! -f dict ]
-	wget http://wordnetcode.princeton.edu/wn3.1.dict.tar.gz
+if [ ! -d dict ]
+then
+	curl -O https://wordnetcode.princeton.edu/wn3.1.dict.tar.gz
 	tar xvfz wn3.1.dict.tar.gz
 fi
 
-if [! -f verbnet3.3 ]
-	wget http://verbs.colorado.edu/verb-index/vn/verbnet-3.3.tar.gz
+if [ ! -d verbnet3.3 ]
+then
+	curl -O https://verbs.colorado.edu/verb-index/vn/verbnet-3.3.tar.gz
 	tar xvfz verbnet-3.3.tar.gz
 fi
 
 
 #build jverbnet from bugfix branch
 cd $OOP_HOME
-if [! -f jverbnet ]
+if [ ! -d jverbnet ]
+then
 	git clone https://github.com/rsadasiv/jverbnet.git --branch bugfix/verbnet33 --single-branch
-	cd jverbnet
-else
-	cd jverbnet
-	git pull
-fi
+fi	
+cd jverbnet
+git pull
 mvn install
 
 
 #build oopcorenlp_parent
 cd $OOP_HOME
-if [! -d oopcorenlp_parent ]
+if [ ! -d oopcorenlp_parent ]
 then
 	git clone https://github.com/rsadasiv/oopcorenlp_parent.git
-	cd oopcorenlp_parent
-else
-	cd oopcorenlp_parent
-	git pull
 fi
-mvn clean install
+cd oopcorenlp_parent
+git pull
+mvn clean install site
 
 #build oopcorenlp
 cd $OOP_HOME
-if [! -d oopcorenlp ]
+if [ ! -d oopcorenlp ]
 then
 	git clone https://github.com/rsadasiv/oopcorenlp.git
-	cd oopcorenlp
-else
-	cd oopcorenlp
-	git pull
 fi
+cd oopcorenlp
+git pull
 mvn clean install site
 
+#build oopcorenlp_corpus
+cd $OOP_HOME
+if [ ! -d oopcorenlp_corpus ]
+then
+	git clone https://github.com/rsadasiv/oopcorenlp_corpus.git
+fi
+cd oopcorenlp_corpus
+git pull
+mvn clean install site
 
+#build oopcorenlp_corpus_cli
+cd $OOP_HOME
+git clone https://github.com/rsadasiv/oopcorenlp_corpus_cli.git
+cd oopcorenlp_corpus_cli
+mvn install site
+
+
+#oopcorenlp.properties must exist in ./Sample/
+#run cli
+cd $OOP_HOME
+if [ ! -d Sample ]
+then
+	mkdir Sample
+	cd Sample
+	mkdir Chekhov
+	mkdir Maupassant
+	mkdir Wodehouse
+	mkdir OHenry
+else
+	rm -Rf ./Sample/Chekhov/*
+	rm -Rf ./Sample/Maupassant/*
+	rm -Rf ./Sample/Wodehouse/*
+	rm -Rf ./Sample/OHenry/*
+fi
+
+cd $OOP_HOME
+java -Xms8096m -Xmx10120m -jar oopcorenlp_corpus_cli/target/oopcorenlp_corpus_cli-$CLI_VERSION.jar --action generate --outputPath ./Sample
+java -Xms8096m -Xmx10120m -jar oopcorenlp_corpus_cli/target/oopcorenlp_corpus_cli-$CLI_VERSION.jar --action analyze --inputPath ./Sample/ChekhovBatch.json
+java -Xms8096m -Xmx10120m -jar oopcorenlp_corpus_cli/target/oopcorenlp_corpus_cli-$CLI_VERSION.jar --action analyze --inputPath ./Sample/MaupassantBatch.json
+java -Xms8096m -Xmx10120m -jar oopcorenlp_corpus_cli/target/oopcorenlp_corpus_cli-$CLI_VERSION.jar --action analyze --inputPath ./Sample/WodehouseBatch.json
+java -Xms8096m -Xmx10120m -jar oopcorenlp_corpus_cli/target/oopcorenlp_corpus_cli-$CLI_VERSION.jar --action analyze --inputPath ./Sample/OHenryBatch.json
+
+#build oopcorenlp_web
+cd $OOP_HOME
+if [ ! -d oopcorenlp_web ]
+then
+	git clone https://github.com/rsadasiv/oopcorenlp_web.git
+fi
+cd oopcorenlp_web
+git pull
+
+#deploy Analyze output to tomcat
+mkdir oopcorenlp_web/WebContent/Corpora/Chekhov
+cp ./Sample/Corpora/Gutenberg/Chekhov/Analyze/* oopcorenlp_web/WebContent/Corpora/Chekhov/
+mkdir oopcorenlp_web/WebContent/Corpora/Maupassant
+cp ./Sample/Corpora/Gutenberg/Maupassant/Analyze/* oopcorenlp_web/WebContent/Corpora/Maupassant/
+mkdir oopcorenlp_web/WebContent/Corpora/Wodehouse
+cp ./Sample/Corpora/EBook/Wodehouse/Analyze/* oopcorenlp_web/WebContent/Corpora/Wodehouse/
+mkdir oopcorenlp_web/WebContent/Corpora/OHenry
+cp ./Sample/Corpora/Wikisource/OHenry/Analyze/* oopcorenlp_web/WebContent/Corpora/OHenry/
+
+#build oopcorenlp
+cd $OOP_HOME
+cd oopcorenlp_web
+mvn clean install site
+
+#corpus cleanup
+#rm -Rf oopcorenlp_web/WebContent/Corpora/*
+
+#publish docs
+cd $OOP_HOME
+if [ ! -d rsadasiv.github.io ]
+then
+	git clone https://github.com/rsadasiv/rsadasiv.github.io.git
+fi
+cd rsadasiv.github.io
+git pull
